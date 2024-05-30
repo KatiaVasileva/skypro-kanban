c96038b4984473d49d1f9afa2f705b34c48e68e6
katiavasileva <eka.vasileva@gmail.com>
2024-05-30 23:38:24 +0500

    Card component is added

---
+++ skypro-kanban/src/data.js
@@ -0,0 +1,14 @@
+export const themes = {
+  green: {
+    cardThemeColor: "_green",
+    cardThemeTitle: "Research",
+  },
+  orange: {
+    cardThemeColor: "_orange",
+    cardThemeTitle: "Web Design",
+  },
+  purple: {
+    cardThemeColor: "_purple",
+    cardThemeTitle: "Copywriting",
+  },
+};
