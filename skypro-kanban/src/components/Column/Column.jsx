a4916785b9c3bc2c09fb74102929e7b9b2a10d07
katiavasileva <eka.vasileva@gmail.com>
2024-06-08 11:51:02 +0500

    minor changes in structure

--- skypro-kanban/src/components/Column/Column.jsx
+++ skypro-kanban/src/components/Column/Column.jsx
@@ -1,20 +1,25 @@
 import PropTypes from "prop-types";
+import Card from "../Card/Card";
 
-function Column({ column, columnTitle, children }) {
+function Column({ column, columnTitle, cards }) {
   return (
     <div className={"main__column " + column}>
       <div className="column__title">
         <p>{columnTitle}</p>
       </div>
-      <div className="cards">{children}</div>
+      <div className="cards">
+        {cards.map((card) => (
+          <Card key={card.id} card={card} />
+        ))}
+      </div>
     </div>
   );
 }
 
 Column.propTypes = {
-    column: PropTypes.string.isRequired,
-    columnTitle: PropTypes.string.isRequired,
-    children: PropTypes.any.isRequired,
-}
+  column: PropTypes.string.isRequired,
+  columnTitle: PropTypes.string.isRequired,
+  cards: PropTypes.array.isRequired,
+};
 
 export default Column;
--- skypro-kanban/src/components/Main/Main.jsx
+++ skypro-kanban/src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import Card from "../Card/Card";
+// import Card from "../Card/Card";
 import { statusList, cardList } from "../../data";
 import Column from "../Column/Column";
 
@@ -9,42 +9,13 @@ function Main() {
         <div className="main__block">
           <div className="main__content">
             {statusList.map((status) => (
-              <Column key={status} column={"column"} columnTitle={status}>
-                {cardList.filter((card) => card.status === status).map((card) => (
-                  <Card key={card.id} card={card} />
-                ))}
-                {/* <Card theme={themes.orange} />
-                <Card theme={themes.green} />
-                <Card theme={themes.orange} />
-                <Card theme={themes.purple} />
-                <Card theme={themes.orange} /> */}
-              </Column>
+              <Column
+                key={status}
+                column={"column"}
+                columnTitle={status}
+                cards={cardList.filter((card) => card.status === status)}
+              ></Column>
             ))}
-            {/* <Column column={"column"} columnTitle={"Без статуса"}>
-              <Card theme={themes.orange} />
-              <Card theme={themes.green} />
-              <Card theme={themes.orange} />
-              <Card theme={themes.purple} />
-              <Card theme={themes.orange} />
-            </Column>
-
-            <Column column={""} columnTitle={"Нужно сделать"}>
-              <Card theme={themes.green} />
-            </Column>
-
-            <Column column={""} columnTitle={"В работе"}>
-              <Card theme={themes.green} />
-              <Card theme={themes.purple} />
-              <Card theme={themes.orange} />
-            </Column>
-
-            <Column column={""} columnTitle={"Тестирование"}>
-              <Card theme={themes.green} />
-            </Column>
-
-            <Column column={""} columnTitle={"Готово"}>
-              <Card theme={themes.green} />
-            </Column> */}
           </div>
         </div>
       </div>
