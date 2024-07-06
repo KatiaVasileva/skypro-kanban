f842155d668d19208dd43d916f08c5550118ffa9
katiavasileva <eka.vasileva@gmail.com>
2024-07-06 13:26:33 +0500

    commit

--- skypro-kanban/src/pages/CardBrowsePage.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-import PopBrowse from "../components/popups/PopBrowse/PopBrowse";
-
-export default function CardBrowsePage() {
-  let { id } = useParams();
-
-  return <PopBrowse cardId={id} />;
-}
